df633d7804ae04cd5c19485cdd23e510c181efe4
Samuel Szoniecky <samszon@gmail.com>
2022-11-02 07:33:47 +0100

    new

--- src/View/Helper/QuerySqlViewHelper.php
+++ src/View/Helper/QuerySqlViewHelper.php
@@ -37,7 +37,7 @@ class QuerySqlViewHelper extends AbstractHelper
                 $result = $this->cooccurrenceValueResource($params);
                 break;                
             case 'tagUses':
-                $result = $this->cooccurrenceValueResource($params);
+                $result = $this->tagUses($params);
                 break;                
             }
 
@@ -52,30 +52,95 @@ class QuerySqlViewHelper extends AbstractHelper
      * @return array
      */
     function tagUses($params){
-        $oClass = $this->api->search('resource_classes', ['term' => $params['class']])->getContent()[0];
+        //récupère le descriptif des usages = le tag utilisé comme valueResource
         $query ="SELECT 
-        r.id,
-        r.title,
-        COUNT(v.id) nbValue,
-        GROUP_CONCAT(DISTINCT p.local_name) props
-    FROM
-        resource r
-            INNER JOIN
-        value v ON v.value_resource_id = r.id
-            INNER JOIN
-        property p ON p.id = v.property_id
-    WHERE
-        r.title like '%?%' 
-         AND (p.local_name = 'hasConcept' OR p.local_name = 'semanticRelation') 
-    GROUP BY r.id ";
-        $having = "";
-        if($params['minVal'] && $params['maxVal'])$having = " HAVING nbValue BETWEEN ".$params['minVal']." AND ".$params['maxVal'];
-        elseif($params['maxVal'])$having = " HAVING nbValue <= ".$params['maxVal'];
-        elseif($params['minVal'])$having = " HAVING nbValue >= ".$params['minVal'];
-        $query .= $having." ORDER BY r.created";
-        $rs = $this->conn->fetchAll($query,[$oClass->id()]);
-        return $rs;       
+                r.id tagId,
+                r.title tagTitle,
+                p.local_name relation,
+                v.resource_id useId,                
+                rc.label useClass
+            FROM
+                resource r
+                    INNER JOIN
+                value v ON v.value_resource_id = r.id
+                    INNER JOIN
+                property p ON p.id = v.property_id
+                    INNER JOIN
+                resource rU ON rU.id = v.resource_id
+                    INNER JOIN
+                resource_class rc ON rc.id = rU.resource_class_id
+            WHERE
+                        r.title like '%".$params['search']."%' 
+                        AND (p.local_name = 'hasConcept' OR p.local_name = 'semanticRelation') 
+                    ";
+                    //GROUP BY r.id, p.local_name ";
+        $query .= " ORDER BY r.created";
+        $rs = $this->conn->fetchAll($query);
+        //détails les usages
+        $tags = [];
+        foreach ($rs as $i=>$r) {
+            //récupère le détail de l'usage suivant sa class
+            if(!isset($tags[$r['tagId']]))
+                $tags[$r['tagId']]=['tagId'=>$r['tagId'],'tagTitle'=>$r['tagTitle'],'relations'=>[]];
+            if(!isset($tags[$r['tagId']]['relations'][$r['useClass']]))
+                $tags[$r['tagId']]['relations'][$r['useClass']]=[];
+            switch ($r['useClass']) {
+                case 'part of speech':
+                    $tags[$r['tagId']]['relations'][$r['useClass']][]=$this->getDetailUsagePartOfSpeech($r['tagId'], $r['useId']);
+                    break;
+            }
+        }
+        return $tags;       
     }
+
+    /**
+     * renvoie le detail des usages pour part of speech
+     *
+     * @param int    $idT identifiant du tag
+     * @param int    $idR identifiant de la ressource
+     * @return array
+     */
+    function getDetailUsagePartOfSpeech($idT, $idR){
+        $query ="SELECT idMin, idMax, nb, pId, pLabel, numVal
+        , vStart.id, vStart.value start
+        , vEnd.id, vEnd.value end
+        , vConf.id, vConf.value confidence
+        , vSpeak.id, vSpeak.value speaker
+        FROM (
+        SELECT 
+            count(v.value_resource_id),
+            count(v.value),
+            min(v.id) idMin,
+            max(v.id) idMax,
+            max(v.id)-min(v.id) nb,
+            v.property_id pId,
+            p.label pLabel,
+            min(vC.id) - min(v.id) numVal 
+        FROM        
+            value v 
+            inner join property p on p.id = v.property_id
+            left join value vC on vC.id = v.id and v.property_id = 2068 and v.value_resource_id = ".$idT."
+        WHERE
+            v.resource_id = ".$idR."
+         group by v.resource_id,  v.property_id
+         having nb > 1 and numVal > 0
+         ) trans,
+         (select id, value, property_id from value WHERE resource_id = ".$idR." and property_id = 208) vStart,
+         (select id, value, property_id from value WHERE resource_id = ".$idR." and property_id = 189) vEnd,
+         (select id, value, property_id from value WHERE resource_id = ".$idR." and property_id = 2043) vConf,
+         (select id, value, property_id from value WHERE resource_id = ".$idR." and property_id = 2082) vSpeak
+        WHERE 
+        vStart.id = trans.idMin+(nb+1)+numVal
+        AND vEnd.id = trans.idMin+((nb+1)*2)+numVal
+        AND vConf.id = trans.idMin+((nb+1)*3)+numVal
+        AND vSpeak.id = trans.idMin+((nb+1)*4)+numVal"; 
+        $rs = $this->conn->fetchAll($query);
+        return $rs;
+    }
+
+    
+    
+
 
     /**
      * renvoie les statistiques d'une class comme valeur de ressource
