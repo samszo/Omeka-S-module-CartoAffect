244635d76f9daf5d637f7bc735dd1a0d112f5fed
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-16 00:00:00 +0000

    Autres nettoyages.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -1,16 +1,30 @@
 <?php declare(strict_types=1);
-namespace Log;
+
+namespace CartoAffect;
+
+
+use Omeka\Module\Exception\ModuleCannotInstallException;
+use Omeka\Stdlib\Message;
 
 /**
  * @var Module $this
- * @var \Laminas\ServiceManager\ServiceLocatorInterface $serviceLocator
+ * @var \Laminas\ServiceManager\ServiceLocatorInterface $services
  * @var string $newVersion
  * @var string $oldVersion
  *
+ * @var \Omeka\Api\Manager $api
+ * @var \Omeka\Settings\Settings $settings
  * @var \Doctrine\DBAL\Connection $connection
+ * @var \Doctrine\ORM\EntityManager $entityManager
+ * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
  */
-$services = $serviceLocator;
-$api = $services->get('Omeka\ApiManager');
+$plugins = $services->get('ControllerPluginManager');
+$api = $plugins->get('api');
+$config = $services->get('Config');
+$settings = $services->get('Omeka\Settings');
+$connection = $services->get('Omeka\Connection');
+$messenger = $plugins->get('messenger');
+$entityManager = $services->get('Omeka\EntityManager');
 
 if (!class_exists(\Generic\InstallResources::class)) {
     if (file_exists(dirname(__DIR__, 3) . '/Generic/InstallResources.php')) {
