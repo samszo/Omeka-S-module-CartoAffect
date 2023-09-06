5decae14e1871662e2a4d121eb6bfc3d29cbe1b0
samszo <samszon@gmail.com>
2023-09-06 17:47:34 +0200

    ajout media and direct query for complexity

--- src/View/Helper/QuerySqlViewHelper.php
+++ src/View/Helper/QuerySqlViewHelper.php
@@ -44,11 +44,148 @@ class QuerySqlViewHelper extends AbstractHelper
                 break;
             case 'tagUses':
                 $result = $this->tagUses($params);
-                break;                
+                break;     
+            case 'propValueResource':
+                $result = $this->propValueResource($params);
+                break;               
+            case 'complexityNbValue':
+                $result = $this->complexityNbValue($params);
+                break;           
+            case 'complexityUpdateValue':
+                $result = $this->complexityUpdateValue($params);
+                break;           
+            case 'complexityInsertValue':
+                $result = $this->complexityInsertValue($params);
+                break;           
+                        
         }
 
         return $result;
 
+    }
+
+    /**
+     * mise à jour de la complexité 
+     *
+     * @param array    $params paramètre de la requête
+     * @return array
+     */
+    function complexityInsertValue($params){
+
+        //création de l'annotation
+        $query ="INSERT INTO `resource` (`owner_id`, `is_public`, `created`,`resource_type`) VALUES
+        (?,1,NOW(),?)";
+        $rs = $this->conn->executeStatement($query,[$params['vals']['owner'],'Omeka\Entity\ValueAnnotation']);
+        $aId = $this->conn->lastInsertId();
+        $query ="INSERT INTO `value_annotation` (`id`) VALUES (".$aId.")";
+        $rs = $this->conn->executeStatement($query);
+
+        //mise à jour de la resource
+        $query ="UPDATE `resource` SET `modified`=NOW() WHERE id =".$params['vals']['id'];
+        $rs = $this->conn->executeStatement($query);
+
+        //création des nouvelles valeurs d'annotation
+        $this->complexityInsertAnnotationValues($aId,$params['vals']);
+
+        //création de la valeur de la ressource
+        $query = "INSERT INTO `value` (`value`,`property_id`, `type`,`resource_id`,`is_public`,`value_annotation_id`)  VALUES
+            (?, ?, 'literal', ?, 1, ?)";
+        $rs = $this->conn->executeStatement($query,[$params['vals']['value'],$params['vals']['property_id'],$params['vals']['id'],$aId]);
+        return $rs;       
+    }
+    /**
+     * ajout de la complexité 
+     *
+     * @param array    $params paramètre de la requête
+     * @return array
+     */
+    function complexityUpdateValue($params){
+        //récupère les identifiants
+        $query ="SELECT id, value_annotation_id FROM value v WHERE v.property_id = ? AND v.resource_id = ?";
+        $rs = $this->conn->fetchAll($query,[$params['vals']['property_id'],$params['vals']['id']]);
+        $aId = $rs[0]['value_annotation_id'];
+        $vId = $rs[0]['id'];
+
+        //mise à jour les resources
+        $query ="UPDATE `resource` SET `modified`=NOW() WHERE id IN (".$params['vals']['id'].",".$aId.")";
+        $rs = $this->conn->executeStatement($query);
+
+        //supression des valeurs de l'annotation
+        $query ="DELETE FROM `value` WHERE resource_id = ".$aId;
+        $rs = $this->conn->executeStatement($query);
+
+        //création des nouvelles valeurs
+        $this->complexityInsertAnnotationValues($aId,$params['vals']);
+
+        //mise à jour de la valeur de la ressource
+        $query ="UPDATE value v SET v.value = ? WHERE v.id = ?";
+        $rs = $this->conn->executeStatement($query,[$params['vals']['value'],$vId]);
+        return $rs;       
+    }
+
+    /**
+     * ajout des annotation de la complexité 
+     *
+     * @param array    $vals paramètre de la requête
+     * @return array
+     */
+    function complexityInsertAnnotationValues($id,$vals){
+
+        $query = "INSERT INTO `value` (`value`, `property_id`, `type`, `resource_id`,`is_public`) VALUES (?, ?, ?, ?, 1)";
+        foreach ($vals['@annotation'] as $a) {
+            foreach ($a as $v) {
+                $rs = $this->conn->executeStatement($query,
+                    [$v['@value'],$v['property_id'],$v['type'],$id]);
+            }
+        }
+    }
+
+
+    /**
+     * renvoie le nombre de ressource par complexité
+     *
+     * @param array    $params paramètre de la requête
+     * @return array
+     */
+    function complexityNbValue($params){
+        $query ="    SELECT 
+                CAST(v.value AS INTEGER) val, COUNT(*) nb
+            FROM
+                property p
+                    INNER JOIN
+                value v ON v.property_id = p.id
+            WHERE
+                p.local_name = 'complexity'
+            GROUP BY v.value
+            ORDER BY val DESC";
+        $rs = $this->conn->fetchAll($query);
+        return $rs;       
+    }
+
+
+    /**
+     * renvoie les propriété utilisées pour les valeurs de ressource
+     *
+     * @param array    $params paramètre de la requête
+     * @return array
+     */
+    function propValueResource($params){
+        $query ="SELECT 
+                v.property_id,
+                COUNT(v.id),
+                CONCAT(vo.prefix, ':', p.local_name) prop
+            FROM
+                value v
+                    INNER JOIN
+                property p ON p.id = v.property_id
+                    INNER JOIN
+                vocabulary vo ON vo.id = p.vocabulary_id
+            WHERE
+                v.value_resource_id IS NOT NULL
+            GROUP BY v.property_id
+                ";
+        $rs = $this->conn->fetchAll($query);
+        return $rs;       
     }
 
     /**
@@ -58,6 +195,8 @@ class QuerySqlViewHelper extends AbstractHelper
      * @return array
      */
     function statResUsed($params){
+        //if(!$this->conn->isConnected())$this->conn->connect();
+
         //ATTENTION: on ne prend pas en compte toutes les ressources mais uniquement certains types
         $resourceTypes = ["Annotate\Entity\Annotation","Omeka\Entity\Item","Omeka\Entity\Media","Omeka\Entity\ItemSet"];
         $query ="SELECT 
@@ -72,6 +211,8 @@ class QuerySqlViewHelper extends AbstractHelper
                 GROUP_CONCAT(v.value_resource_id) idsRes,
                 GROUP_CONCAT(CONCAT(vo.prefix, ':', p.local_name)) propsRes,
                 COUNT(v.value_annotation_id) nbAno,
+                COUNT(DISTINCT m.id) nbMedia,
+                GROUP_CONCAT(DISTINCT m.id) idsMedia,
                 r.resource_type,
                 rc.label 'class label',
                 rc.id 'idClass'
@@ -79,6 +220,8 @@ class QuerySqlViewHelper extends AbstractHelper
                 value v
                     INNER JOIN
                 resource r ON r.id = v.resource_id
+                    LEFT JOIN
+                media m ON m.item_id = r.id
                     LEFT JOIN
                 resource_class rc ON rc.id = r.resource_class_id
                     LEFT JOIN
@@ -116,6 +259,7 @@ class QuerySqlViewHelper extends AbstractHelper
                 GROUP BY r.id ";
             $rs = $this->conn->fetchAll($query,$resourceTypes);
         }
+        //$this->conn->close();
         return $rs;       
     }
 
